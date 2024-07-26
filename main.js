df6438b51d5b03e245b12f18d2da09d1d0af468a
bk <kblessingtatenda@gmail.com>
2024-07-27 01:14:24 +0200

    fix cors

--- main.js
+++ main.js
@@ -4,11 +4,13 @@ import cors from 'cors'
 import axios from 'axios'
 import { makeCodeChanges } from './src/controllers/changeController.js'
 import { fixCode } from './src/controllers/fixCode.js'
+import bodyParser from 'body-parser'
 
 const app = express()
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(bodyParser.json());
+// app.use(express.json())
 
 // app.post('/fixr', async (req, res) => {
 //   const eslint = new ESLint()
@@ -40,7 +42,10 @@ app.use(express.json())
 
 app.post('/fix-code', fixCode);
 app.post('/pull-request', makeCodeChanges);
-
+app.get('/', function(req, res) {
+  console.log('works')
+  res.send('works')
+})
 app.listen(3000, () => {
   console.log('Server listening on port: 3000')
 })
